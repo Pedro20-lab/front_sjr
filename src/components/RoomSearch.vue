78b040aed3f5bd1bf5c90c550dea5c6ed0a09a5f
Pedro Burgos <pedroburgos988@gmail.com>
2026-03-19 15:23:02 -0500

    Failed changes

---
+++ src/components/RoomSearch.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { ref } from 'vue';
+    const checkin = ref()
+    const checkout = ref()
+</script>
+
+<template>
+  <label for="checkin">Entrada</label>
+  <input id="checkin" name="checkin" type="date" value="{{ checkin }}">
+
+  <label for="checkout">Salida</label>
+  <input name="checkout" type="date" value="{{ checkout }}" >
+
+</template>
